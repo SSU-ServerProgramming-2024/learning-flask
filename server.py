edc7ac83be07c5c7d43576fe2d9b7ba420618411
sshhlll <tvrla@naver.com>
2024-03-31 14:02:04 +0900

    homework

---
+++ server.py
@@ -0,0 +1,72 @@
+from flask import Flask
+import random
+
+app = Flask(__name__)
+
+topics = [
+    {'id':1, 'title':'html', 'body':'html is ...'},
+    {'id':2, 'title':'css', 'body':'css is ...'},
+    {'id':3, 'title':'javascript', 'body':'javascript is ...'}
+
+]
+
+def template(contents, content):
+    return f'''<!doctype html>
+    <html>
+        <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+                {contents}
+            </ol>
+                {content}
+        </body>
+    </html>
+    '''
+
+
+def getContents():
+    liTags = ''
+    for topic in topics:
+        liTags = liTags + f'<li><a href="/read/{topic["id"]}/">{topic["title"]}</a></li>'
+    return liTags
+
+
+
+@app.route('/')
+def index():
+    return template(getContents(),'<h2>Welcome</h2>Hello, Web')
+
+
+@app.route('/create/')
+def create():
+    return 'Create'
+
+
+
+@app.route('/read/<int:id>/')
+def read(id):
+    liTags = ''
+    for topic in topics:
+        liTags = liTags + f'<li><a href="/read/{topic["id"]}/">{topic["title"]}</a></li>'
+    title = ''
+    body =''
+    for topic in topics:
+        if id == topic['id']:
+            title = topic['title']
+            body = topic['body']
+            break
+    return f'''<!doctype html>
+    <html>
+        <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+                {liTags}
+            </ol>
+            <h2>{title}</h2>
+            {body}
+        </body>
+    </html>
+    '''
+    return template(getContents(),'<h2>{title}</h2>{body}')
+
+app.run(debug=True)
