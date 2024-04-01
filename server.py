b688de0d7e48d1aa6739c410fc5173f833a8df1a
sshhlll <tvrla@naver.com>
2024-04-01 23:21:58 +0900

    0401

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
-from flask import Flask
-import random
-
+from flask import Flask, request
 app = Flask(__name__)
 
+
+nextId = 4
 topics = [
     {'id':1, 'title':'html', 'body':'html is ...'},
     {'id':2, 'title':'css', 'body':'css is ...'},
@@ -18,7 +18,10 @@ def template(contents, content):
             <ol>
                 {contents}
             </ol>
-                {content}
+            {content}
+            <ul>
+                <li><a href="/create/">create</a><li>
+            </ul>
         </body>
     </html>
     '''
@@ -37,9 +40,31 @@ def index():
     return template(getContents(),'<h2>Welcome</h2>Hello, Web')
 
 
-@app.route('/create/')
+
+
+@app.route('/create/', methods=['GET','POST'])
 def create():
-    return 'Create'
+    if request.method == 'GET':
+        content = '''
+            <form action="/create/" method="POST">
+                <p><input type="text" name="title" placeholder="title"></p>
+                <p><textarea name="body" placeholder="body"></textarea></p>
+                <p><input type="submit" value="create"></p>
+            </form>
+        '''
+        return template(getContents(),content)
+    elif request.method == 'POST':
+        global nextId
+        title = request.form['title']
+        body = request.form['body']
+        newTopic = {'id': nextId, 'title': title, 'body':body}
+        topics.append(newTopic)
+        url = '/read/'+str(nextId)+'/'
+        nextId=nextId+1
+        return title+','+ body
+
+
+
 
 
 
